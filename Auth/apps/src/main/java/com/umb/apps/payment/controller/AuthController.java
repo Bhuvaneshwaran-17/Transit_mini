93dd56141babaede17f89e2fff35f5ef2565ebbe
CARCGNT\221134 <Bhuvaneshwaran.G@cubic.com>
2026-03-19 12:47:07 +0530

    added errors for the ai integration 12:47

--- Auth/apps/src/main/java/com/umb/apps/payment/controller/AuthController.java
+++ Auth/apps/src/main/java/com/umb/apps/payment/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Slf4j
 public class AuthController {
-jk
+j
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserRepository userRepository;
